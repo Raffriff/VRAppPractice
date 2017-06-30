fbd9e5039d4f6c1c3e485ac2d4b89644e5e29cf5
Dave <dave.donovan@hotmail.com>
2017-06-30 13:13:00 +0100

    EULA Page
    (Trigger Fixed)

--- app/src/main/java/com/example/david/vrapppractice/EULA.java
+++ app/src/main/java/com/example/david/vrapppractice/EULA.java
@@ -2,8 +2,6 @@ package com.example.david.vrapppractice;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,7 +20,7 @@ public class EULA extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eul);
+        setContentView(R.layout.activity_eula);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
